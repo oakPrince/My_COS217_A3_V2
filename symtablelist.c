e1fb039c78572a55248233a07cc4a74c8b870dc7
Okezie J. Eze <oe7003@armlab01.cs.princeton.edu>
2023-03-25 02:49:59 -0400

    first four functions in symtablelist.c

--- symtablelist.c
+++ symtablelist.c
@@ -19,7 +19,7 @@ struct Node
 };
 
 /* Begins linkedlist by pointing to first node */
-struct List
+struct SymTable
 {
   struct Node *first;
 };
@@ -57,3 +57,57 @@ void SymTable_free(SymTable_T oSymTable)
   free(oSymTable);
     
 }
+
+/* The function takes one arguement, oSymTable which is of type SymTable_T, and returns the number of bindings in oSymTable. */
+
+size_t SymTable_getLength(SymTable_T oSymTable)
+{
+  size_t numOfBindings = 0;
+  struct Node *psCurrentNode; 
+  struct Node *psNextNode;
+    
+  assert(oSymTable != NULL);
+
+  for(psCurrentNode = oSymTable->first;
+      psCurrentNode != NULL;
+      psCurrentNode = psNextNode)
+  {
+    if(psCurrentNode.key != NULL && psCurrentNode.value != NULL)
+    {
+      numOfBindings++;
+    }
+    psNextNode = psCurrentNode->psNextNode;
+  }
+
+  return numOfBindings;
+  
+}
+
+/* The function SymTable_put takes three arguments: SymTable_T type oSymTable, constnat char pointer type pcKey, and constant pointer type pvValue. oSymTalbe is searched for if it contains key pcKey. If it does the binding is replaced with value pvValue. If not, oSymTable is not changed and the function returns NULL.  */
+
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
+{
+  struct Node *psCurrentNode;
+  struct Node *psNextNode;
+  int oldVal;
+  
+  assert(oSymTable != NULL);
+
+  for (psCurrentNode = oSymTable->first;
+       psCurrentNode != NULL;
+       psCurrentNode = psNextNode)
+  {
+    if(psCurrentNode.key == pcKey)
+    {
+      oldVal = psCurrentNode.value;
+      psCurrentNode.value = pvValue;
+      return oldVal;
+    }
+    psNextNode = psCurrentNode->psNextNode;
+  }
+
+  return NULL;
+  
+}
+
+/* SymTable   */
