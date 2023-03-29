420bb8023321618bce487e74aa01c877843763bd
Okezie J. Eze <oe7003@armlab01.cs.princeton.edu>
2023-03-29 13:55:10 -0400

    symtablehash more edits

--- symtablehash.c
+++ symtablehash.c
@@ -27,6 +27,8 @@ struct Table
   size_t length;
   /* struct Binding begins hash table */
   struct Binding **buckets;
+  /* Number of buckets in the Symtable  */
+  size_t uBucketCount = initialBucketCount;
 }
 
 /* Return a hash code for pcKey that is between 0 and uBucketCount-1,
@@ -54,9 +56,78 @@ SymTable_T SymTable_new(void)
     return NULL;
   }
 
-  /* */
-  for (int i = 0; i <
-  oSymTable->buckets[i]
+  /* set each bucket to null */
+  for (int i = 0; i < uBucketCount; i++)
+  {
+    oSymTable->buckets[i] = NULL;
+  }
   oSymTable->length = 0;
-  oSymTable->first
+  
+  return oSymTable;
+}
+
+SymTable_T SymTable_free(SymTable_T oSymTable)
+{
+  struct Binding *current;
+  struct Binding *forward;
+
+  assert(oSymTable != NULL);
+  
+  for (int i = 0; i < uBucketCount; i++)
+  {
+    for (current = oSymTable->first; current != NULL; current = forward)
+    {
+      forward = current->next;
+      free((void*) current->key);
+      free(current);
+    }
+  }
+
+  free(oSymTable);
+}
+
+size_t SymTable_getLength(SymTable_T oSymTable)
+{
+  assert(oSymTable != NULL);
+  
+  return oSymTable->length;
+}
+
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
+{
+  
+  assert(oSymTable != NULL):
+  assert(pcKey != NULL);
+
+  
+}
+
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
+{
+  assert(assert(oSymTable != NULL):
+  assert(pcKey != NULL);
+}
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey)
+{
+  assert(oSymTable != NULL):
+  assert(pcKey != NULL);
+}
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey)
+{
+  assert(oSymTable != NULL):
+  assert(pcKey != NULL);
+}
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey)
+{
+  assert(oSymTable != NULL):
+  assert(pcKey != NULL);
+}
+
+void SymTable_map(SymTable_T oSymTable, void(*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), const void *pvExtra)
+{
+  assert(oSymTable != NULL):
+  assert(pfApply != NULL);
 }
